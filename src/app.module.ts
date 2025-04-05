8b0db544e60677144c2e903f12e26ad771e41dba
AleksandroCalamani <aleksandro.calamani11@gmail.com>
2025-04-05 11:37:29 +0200

    DB Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'teshotel-management',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+    TestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
