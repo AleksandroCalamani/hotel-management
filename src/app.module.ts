7dd998461a5747879c0b978ddb4dbe6c32a01b52
AleksandroCalamani <aleksandro.calamani11@gmail.com>
2025-04-05 11:41:02 +0200

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
@@ -17,8 +15,6 @@ import { TestModule } from './test/test.module';
       entities: [],
       synchronize: true,
     }),
-    UserModule,
-    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
