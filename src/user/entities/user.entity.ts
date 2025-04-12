724d54973519e9ac03fc8e0e4df2e4fe89f7bf70
AleksandroCalamani <aleksandro.calamani11@gmail.com>
2025-04-12 11:32:00 +0200

    Add User Attributes

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,53 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
+  id: number;
+
+  @Column('text', {
+    name: 'first_name',
+    nullable: false,
+  })
+  firstname: string;
+
+  @Column('text', {
+    name: 'last_name',
+    nullable: false,
+  })
+  lastname: string;
+
+  @Column('text', {
+    name: 'email',
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+
+  @Column('text', {
+    name: 'password',
+    nullable: false,
+  })
+  password: string;
+
+  @Column('datetime', {
+    name: 'birthday',
+    nullable: false,
+  })
+  birthday: Date;
+
+  @Column('datetime', {
+    name: 'created_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column('datetime', {
+    name: 'updated_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
