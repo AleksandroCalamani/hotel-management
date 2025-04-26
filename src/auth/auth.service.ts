f182965bb1b346a4c715a6bd8da3ec7a2b03758b
AleksandroCalamani <aleksandro.calamani11@gmail.com>
2025-04-26 11:01:53 +0200

    8 - Authentication

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  create(createAuthDto: LoginDto) {
+    return 'This action adds a new auth';
+  }
+
+ 
+}
