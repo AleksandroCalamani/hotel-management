c2a3035d8fcb2f424be8f6ba97f0d22e292757c2
AleksandroCalamani <aleksandro.calamani11@gmail.com>
2025-05-17 10:44:23 +0200

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { Room } from './room/entities/room.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -22,5 +24,7 @@ import { Room } from './room/entities/room.entity';
     UserModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
